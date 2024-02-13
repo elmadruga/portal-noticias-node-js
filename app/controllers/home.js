bc58b8e0a28df5c31f5e1c49529953191c7bd6e7
elmadruga <momo.ju@hotmail.com>
2024-02-13 12:01:02 -0300

    refactor-home.js

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,12 @@
 module.exports.index = function(app, req, res) {
-    res.render('home/index');
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    // Recupera as 5 últimas notícias
+    noticiasModel.get5UltimasNoticias(function(error, result) {
+        res.render('home/index', {noticias: result});
+    });
+
+    
 }
